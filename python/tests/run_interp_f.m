29f0a8835e44b1b69116b21b03ead403a74d8fcc
Feilong Ma <mafeilong@gmail.com>
2015-07-30 13:29:15 -0400

    Add more test cases to interp_f

--- python/tests/run_interp_f.m
+++ python/tests/run_interp_f.m
@@ -19,3 +19,28 @@ for coord_n = 1:2
         end
     end
 end
+
+rng(0);
+n_nodes = 1000;
+
+cart_coords = rand(3, n_nodes);
+coord_maps = randi([1, 3], 1, n_nodes);
+spher_coords = computeSphericalFromCartesian(cart_coords, coord_maps);
+coords1 = computeCartesianFromSpherical(spher_coords, coord_maps) * 0.1;
+
+%% total_nbrs = randi([1, 6], 1, n_nodes);
+%% nbrs = -99 * ones(6, n_nodes);
+%% for i = 1:n_nodes
+%%     n_nbrs = total_nbrs(i);
+%%     nbrs(1:n_nbrs, i) = datasample(0:(n_nodes-1), n_nbrs);
+%% end
+
+coords2 = coords1(:,randperm(size(coords1,2)));
+
+resolution = 2;
+
+gds = computeGeodesicDistances(coords1, coords2);
+[interp_vals, non_zero_locs] = interp_f(coords1, coords2, resolution);
+
+fname = 'data_for_test_interp_f.mat';
+save(fname, 'coords1', 'coords2', 'interp_vals', 'non_zero_locs', 'resolution', 'gds');
--- python/tests/test_interp_f.py
+++ python/tests/test_interp_f.py
@@ -8,7 +8,6 @@ DIR = os.path.dirname(os.path.realpath(__file__))
 
 
 def test_gds_to_interp_vals():
-    # TODO: better test cases (non-empty non_zero_locs etc.)
     for coord in ['cartesian', 'spherical']:
         for submode in ['single', 'multi']:
             for i in range(20):
@@ -20,6 +19,13 @@ def test_gds_to_interp_vals():
                     gds=data['gds'].ravel(), resolution=2, zM=data['zM']-1)
                 assert_allclose(interp_vals, data['interp_vals'].ravel())
                 assert_allclose(non_zero_locs, data['non_zero_locs'].ravel())
+
+    fname = os.path.join(DIR, 'data_for_test_interp_f.mat')
+    mat = loadmat(fname)
+    interp_vals, non_zero_locs = gds_to_interp_vals(
+        mat['gds'].ravel(), float(mat['resolution']))
+    assert_allclose(interp_vals, mat['interp_vals'].ravel())
+    assert_allclose(non_zero_locs, mat['non_zero_locs'].ravel()-1)
 
 
 def test_interp_f():
@@ -35,3 +41,10 @@ def test_interp_f():
                     gds=data['gds'].ravel(), zM=data['zM']-1)
                 assert_allclose(interp_vals, data['interp_vals'].ravel())
                 assert_allclose(non_zero_locs, data['non_zero_locs'].ravel())
+
+    fname = os.path.join(DIR, 'data_for_test_interp_f.mat')
+    mat = loadmat(fname)
+    interp_vals, non_zero_locs = interp_f(
+        mat['coords1'], mat['coords2'], float(mat['resolution']))
+    assert_allclose(interp_vals, mat['interp_vals'].ravel())
+    assert_allclose(non_zero_locs, mat['non_zero_locs'].ravel()-1)
