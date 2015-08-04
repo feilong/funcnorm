c1d1935a3beda7bf4026355a90acca7b16a32f45
Feilong Ma <mafeilong@gmail.com>
2015-08-04 13:08:40 -0400

    Add compute_correlation_cost

---
+++ python/compute_correlation_cost.py
@@ -0,0 +1,79 @@
+import numpy as np
+from compute_geodesic_distances import compute_geodesic_distances
+from derivatives import f_derivatives
+from interp_f import gds_to_interp_vals
+
+
+def compute_correlation_cost(V1ST, coords, nbrs, total_nbrs, resolution,
+                             W2TU1, warp_coords, thr=1e-8, dtype='float',
+                             compute_derivatives=True):
+    """
+    Parameters
+    ----------
+    V1ST: (K, n_nodes) array
+    W2TU1: (n_timepoints, K) array
+    coords : (2, n_nodes) array
+    warp_coords : (2, n_nodes) array
+        If it's None, `coords` would be used.
+        If it's an array, its shape should be the same as `coords`.
+    nbrs : (max_nbrs, n_nodes) array
+    total_nbrs : (n_nodes, ) array
+    resolution : float?
+    thr : float
+
+    Returns
+    -------
+    S : float?
+    dS_dphi : (n_nodes, ) array
+        Is this dS_dphi or -dS_dphi?
+    dS_dtheta : (n_nodes, ) array
+
+    """
+    n_nodes = V1ST.shape[1]
+    S = 0.0
+    if compute_derivatives:
+        dS_dphi = np.zeros((n_nodes, ))
+        dS_dtheta = np.zeros((n_nodes, ))
+    # Q = np.zeros(V1ST.shape, dtype=dtype)
+    # if compute_derivatives:
+    #     dAD_dphi = np.zeros(nbrs.shape, dtype=dtype)
+    #     dAD_dtheta = np.zeros(nbrs.shape, dtype=dtype)
+
+    for j in range(n_nodes):
+        curr_coords = warp_coords[:, [j]]
+        curr_nbrs = nbrs[:total_nbrs[j], j]
+        nbr_coords = coords[:, curr_nbrs]
+
+        gds = compute_geodesic_distances(curr_coords, nbr_coords)
+        A, non_zero_locs = gds_to_interp_vals(gds, resolution)
+        A = A[non_zero_locs]
+        curr_nbrs = curr_nbrs[non_zero_locs]
+        nbr_coords = nbr_coords[:, non_zero_locs]
+        gds = gds[non_zero_locs]
+        Q = V1ST[:, curr_nbrs].dot(A)
+        qnorm = np.linalg.norm(Q)
+
+        if qnorm < thr:
+            continue  # ignore node j
+
+        Q /= qnorm
+        D = 1.0 / qnorm
+        S += 1.0 - W2TU1[j, :].dot(Q)
+
+        if not compute_derivatives:
+            continue
+
+        dA_dphi, dA_dtheta = f_derivatives(
+            curr_coords, nbr_coords, resolution, gds)
+        Q_V1S = Q.dot(V1ST[:, curr_nbrs])
+        # dA_dphi: (n_nbrs, ); Q_V1S: (n_nbrs, ); A: (n_nbrs, );
+        # dAD_dphi: (n_nbrs, )
+        dAD_dphi = D * (dA_dphi - D * Q_V1S.dot(dA_dphi) * A)
+        dAD_dtheta = D * (dA_dtheta - D * Q_V1S.dot(dA_dtheta) * A)
+        dS_dAD = -W2TU1[j, :].dot(V1ST[:, curr_nbrs])  # (n_nbrs, )
+        dS_dphi[j] = dS_dAD.dot(dAD_dphi)
+        dS_dtheta[j] = dS_dAD.dot(dAD_dtheta)
+
+    if compute_derivatives:
+        return S, dS_dphi, dS_dtheta
+    return S
---
+++ python/tests/test_compute_correlation_cost.py
@@ -0,0 +1,104 @@
+from nose.tools import assert_true
+from numpy.testing import assert_allclose, assert_almost_equal
+import numpy as np
+
+from ..blur_dataset import blur_dataset_no_svd
+from ..compute_correlation_cost import compute_correlation_cost
+from ..compute_spherical_from_cartesian import compute_spherical_from_cartesian
+
+
+def generate_random_dataset(seed, n_nodes, n_timepoints):
+    np.random.seed(seed)
+    ds = np.random.random((n_timepoints, n_nodes))
+    ds /= np.linalg.norm(ds, axis=0)
+    return ds
+
+
+def generate_random_nbrs(seed, n_nodes, max_nbrs=6):
+    np.random.seed(seed)
+    nbrs = -99 * np.ones((max_nbrs, n_nodes), dtype='int')
+    total_nbrs = np.zeros((n_nodes, ))
+    for j in range(n_nodes):
+        n_nbrs = np.random.randint(1, max_nbrs+1)
+        nbrs[:n_nbrs, j] = np.random.choice(range(n_nodes), (n_nbrs, ), False)
+        total_nbrs[j] = n_nbrs
+    return nbrs, total_nbrs
+
+
+def generate_random_spher_coords(seed, n_nodes):
+    np.random.seed(seed)
+    cart_coords = np.random.random((3, n_nodes))
+    spher_coords = compute_spherical_from_cartesian(cart_coords, 1)
+    return spher_coords
+
+
+def test_compute_correlation_cost():
+    n_nodes = 300
+    n_timepoints = 100
+    ds1 = generate_random_dataset(0, n_nodes, n_timepoints)
+    ds2 = generate_random_dataset(1, n_nodes, n_timepoints)
+    nbrs, total_nbrs = generate_random_nbrs(0, n_nodes)
+    coords = generate_random_spher_coords(0, n_nodes)
+
+    U, s, Vt = np.linalg.svd(ds1, full_matrices=False)
+    V1ST = np.tile(s[:, np.newaxis], (1, n_nodes)) * Vt
+    W2TU1 = ds2.T.dot(U)
+    assert_allclose(W2TU1.dot(V1ST), ds2.T.dot(ds1))
+    S = compute_correlation_cost(
+        V1ST, coords, nbrs, total_nbrs, 2, W2TU1, coords,
+        compute_derivatives=False)
+    assert_true(0 < S < n_nodes)
+    Q = blur_dataset_no_svd(Vt.T, s, coords, nbrs, total_nbrs, 2)
+    ds3 = U.dot(Q)
+    idx = np.where(np.linalg.norm(ds3, axis=0))[0]
+    S2 = 1 - np.sum((ds3 * ds2)[:, idx], axis=0)
+    assert_almost_equal(S2.sum(), S)
+
+
+def test_compute_correlation_cost_derivatives():
+    for seed in range(10):
+        print seed,
+        _test_compute_correlation_cost_derivatives(seed)
+    print 'Finished testing compute_correlation_cost'
+
+
+def _test_compute_correlation_cost_derivatives(seed=0, atol=1e-5, rtol=1e-4):
+    n_nodes = 300
+    n_timepoints = 100
+    ds1 = generate_random_dataset(seed, n_nodes, n_timepoints)
+    ds2 = generate_random_dataset(1, n_nodes, n_timepoints)
+    nbrs, total_nbrs = generate_random_nbrs(seed, n_nodes, n_timepoints)
+    coords = generate_random_spher_coords(seed, n_nodes)
+
+    U, s, Vt = np.linalg.svd(ds1, full_matrices=False)
+    V1ST = np.tile(s[:, np.newaxis], (1, n_nodes)) * Vt
+    W2TU1 = ds2.T.dot(U)
+    S, dS_dphi, dS_dtheta = compute_correlation_cost(
+        V1ST, coords, nbrs, total_nbrs, 2, W2TU1, coords)
+
+    dS_dphi2 = np.zeros(dS_dphi.shape)
+    dS_dtheta2 = np.zeros(dS_dtheta.shape)
+
+    delta = 1e-8
+    coords2 = coords.copy()
+    for j in range(n_nodes):
+        coords2[0, j] += delta
+        S2 = compute_correlation_cost(V1ST, coords, nbrs, total_nbrs, 2, W2TU1,
+                                      coords2, compute_derivatives=False)
+        dS_dphi2[j] = (S2 - S) / delta
+        coords2[0, j] = coords[0, j]
+
+        coords2[1, j] += delta
+        S2 = compute_correlation_cost(V1ST, coords, nbrs, total_nbrs, 2, W2TU1,
+                                      coords2, compute_derivatives=False)
+        dS_dtheta2[j] = (S2 - S) / delta
+        coords2[1, j] = coords[1, j]
+
+    # Q = blur_dataset_no_svd(Vt.T, s, coords, nbrs, total_nbrs, 2)
+    # ds3 = U.dot(Q)
+    # idx = np.where(np.linalg.norm(ds3, axis=0))[0]
+    # for j in idx:
+    #     print '%5s,%4d,%10f,%10f' % ('phi', j, dS_dphi[j], dS_dphi2[j])
+    #     print '%5s,%4d,%10f,%10f' % ('theta', j, dS_dtheta[j], dS_dtheta2[j])
+    assert_allclose(dS_dphi, dS_dphi2, atol=atol, rtol=rtol)
+    assert_allclose(dS_dtheta, dS_dtheta2, atol=atol, rtol=rtol)
